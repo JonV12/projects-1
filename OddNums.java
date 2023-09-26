088b31c4f6faec0269fc904f897a4805aeccedaa
JonV12 <146036546+JonV12@users.noreply.github.com>
2023-09-26 10:30:35 -0400

    Made to print on new lines

--- OddNums.java
+++ OddNums.java
@@ -2,7 +2,7 @@ public class OddNums {
     public static void main(String[]args){
         int i = 1;
         do{
-            System.out.print(i + " ");
+            System.out.println(i);
             i+=2;
         }while(i < 100);
     }
