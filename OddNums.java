0644a4288cf745dbce94ba483ff6a8f758d75b31
JonV12 <146036546+JonV12@users.noreply.github.com>
2023-09-26 10:36:12 -0400

    Changed to For loop

--- OddNums.java
+++ OddNums.java
@@ -1,9 +1,16 @@
 public class OddNums {
     public static void main(String[]args){
+        /* 
         int i = 1;
         do{
             System.out.println(i);
             i+=2;
         }while(i < 100);
+        */
+
+        for(int i = 1; i <100; i++){
+            System.out.println(i);
+            i++;
+        }
     }
 }
