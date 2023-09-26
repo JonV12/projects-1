86f59e1d8e2454bf0f18058cd108b042be6e6ba8
JonV12 <146036546+JonV12@users.noreply.github.com>
2023-09-26 14:41:04 -0400

    Tree planting project completed

--- Tree.java
+++ Tree.java
@@ -1,1 +1,16 @@
+public class Tree{
+    public static void main(String[] args){
+        //In this program we'll introduce ourselves and print a Tree to the screen
+        System.out.println("HI! I'm Jonathan and I'm currently learing to code Java with Codecademy.");
+        System.out.println("Now let's plant a tree.");
 
+        //Tree
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println("*****");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("=====");
+
+    }
+}
