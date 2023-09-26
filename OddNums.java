5d4c337250ca745150b6f7a47d0f1cbadeba66fc
JonV12 <146036546+JonV12@users.noreply.github.com>
2023-09-26 10:43:35 -0400

    Added modulus validation for odd numbers

--- OddNums.java
+++ OddNums.java
@@ -1,6 +1,6 @@
 public class OddNums {
     public static void main(String[]args){
-        /* 
+        /* v1
         int i = 1;
         do{
             System.out.println(i);
@@ -8,9 +8,19 @@ public class OddNums {
         }while(i < 100);
         */
 
+        /* v2
         for(int i = 1; i <100; i++){
             System.out.println(i);
             i++;
         }
+        */
+
+        //v 2.1 - using modulus validation for odd numbers
+        for(int i = 1; i <100; i++){
+            if(i % 2 == 1){
+                System.out.println(i);
+            }
+        }
+
     }
 }
