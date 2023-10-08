1ec025dd0e15574591f22d0d28f8005c4fb320fd
JonV12 <146036546+JonV12@users.noreply.github.com>
2023-10-08 13:53:25 -0400

    Create MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,10 @@
+public class MyProfile{
+    public static void main(String[] args){
+        String name = "Jonathan";
+        int age = 22;
+        double desiredSalary = 65000.00;
+        char gender = 'm';
+        boolean lookingForJob = false;
+
+    }
+}
