e80b4fdcca2560e587c5af2843a0ea74c6890d1f
JonV12 <146036546+JonV12@users.noreply.github.com>
2023-10-08 20:32:16 -0400

    Create Magic.java

---
+++ Magic.java
@@ -0,0 +1,14 @@
+public class Magic{
+    public static void main(String[] args){
+        // myNumber will be the original number throughout the code
+        int myNumber = 3;
+        int stepOne = myNumber * myNumber;
+        int stepTwo = step1 = myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive / 6;
+        System.out.println(stepSix);
+
+    }
+}
