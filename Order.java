f204529303226b1ad1e9bf4b1340b817bff17a9d
JonV12 <146036546+JonV12@users.noreply.github.com>
2023-10-11 15:09:04 -0400

    Update Order.java
    Write an if-then-else statement that:
    When isFilled is true, print Shipping.
    When isFilled is false, print Order not ready.

--- Order.java
+++ Order.java
@@ -2,5 +2,9 @@ public class Order{
     public static void main(String[]args){
         double itemCost = 30.99;
         if(itemCost > 24.00) System.out.println("High value item!");
+
+        boolean isFilled = false;
+        if(isFilled) System.out.println("Shipping");
+        else System.out.println("Order not ready");
     }
 }
